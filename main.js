b15687bcdc9938227b53d6342216d2c1e8f40459
shubhamchauhan1204.99@gmail.com <shubhamchauhan1204.99@gmail.com>
2023-09-26 21:09:23 +0530

    function written for QR generate

--- main.js
+++ main.js
@@ -3,4 +3,26 @@ const sizes = document.querySelector('#sizes');
 const generateBtn = document.querySelector('#genBtn');
 const downloadBtn = document.querySelector('#downBtn');
 
+let size = sizes.value;
+
 const qrBody = document.querySelector('.content');
+
+
+generateBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    qrCodeGenerate();
+})
+
+
+
+function qrCodeGenerate(){
+
+    new QRCode(qrBody,{
+        text:qrText.value,
+        height:size,
+        width:size,
+        colorLight:"#ffffff",
+        colorDark:"#000000",
+
+    });
+}
