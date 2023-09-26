36164fd38807eefdd44a1b287dc1c2bf5fb02cb8
shubhamchauhan1204.99@gmail.com <shubhamchauhan1204.99@gmail.com>
2023-09-26 20:53:47 +0530

    variable declaration

---
+++ main.js
@@ -0,0 +1,6 @@
+const qrText = document.querySelector('#qr-text');
+const sizes = document.querySelector('#sizes');
+const generateBtn = document.querySelector('#genBtn');
+const downloadBtn = document.querySelector('#downBtn');
+
+const qrBody = document.querySelector('.content');
